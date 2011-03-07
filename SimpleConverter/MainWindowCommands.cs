7217896392aefb56652ac2af208f544b9261f0f9
Lukáš Černý <cerny.l@gmail.com>
2011-03-07 14:26:28 +0100

    File and folder manipulation in main window

--- SimpleConverter/MainWindowCommands.cs
+++ SimpleConverter/MainWindowCommands.cs
@@ -6,30 +6,84 @@ using System.Windows.Input;
 
 namespace SimpleConverter
 {
-    public class StartConversionCommand : ICommand
+    /// <summary>
+    /// Base class for commands implementation
+    /// </summary>
+    public abstract class BaseCommand : ICommand
     {
-        private readonly MainWindowViewModel _viewModel;
+        protected readonly MainWindowViewModel _viewModel;
 
-        public StartConversionCommand(MainWindowViewModel viewModel)
+        public bool Disabled { get; set; }
+
+        public BaseCommand(MainWindowViewModel viewModel, bool disabled = false)
         {
             _viewModel = viewModel;
+            Disabled = disabled;
         }
 
-        public void Execute(object parameter)
-        {
-            _viewModel.StartConversionClicked();
-        }
+        public abstract void Execute(object parameter);
 
         public bool CanExecute(object parameter)
         {
-            // todo: implement this
-            return true;
+            return !Disabled;
         }
 
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+
+    /// <summary>
+    /// Command handler class for Start conversion button
+    /// </summary>
+    public class StartConversionCommand : BaseCommand
+    {
+        public StartConversionCommand(MainWindowViewModel viewModel, bool disabled = false) : base(viewModel, disabled) { }
+
+        public override void Execute(object parameter)
+        {
+            _viewModel.StartConversionClicked();
+        }
+    }
+
+    /// <summary>
+    /// Command handler class for Add file button
+    /// </summary>
+    public class AddFileCommand : BaseCommand
+    {
+        public AddFileCommand(MainWindowViewModel viewModel, bool disabled = false) : base(viewModel, disabled) { }
+
+        public override void Execute(object parameter)
+        {
+            _viewModel.AddFileClicked();
+        }
+    }
+
+    /// <summary>
+    /// Command handler class for Remove file button
+    /// </summary>
+    public class RemoveFileCommand : BaseCommand
+    {
+        public RemoveFileCommand(MainWindowViewModel viewModel, bool disabled = false) : base(viewModel, disabled) { }
+
+        public override void Execute(object parameter)
+        {
+            _viewModel.RemoveFileClicked();
+        }
+    }
+
+    /// <summary>
+    /// Command handler class for Browse button
+    /// </summary>
+    public class BrowseCommand : BaseCommand
+    {
+        public BrowseCommand(MainWindowViewModel viewModel, bool disabled = false) : base(viewModel, disabled) { }
+
+        public override void Execute(object parameter)
+        {
+            _viewModel.BrowseClicked();
         }
     }
 }
--- SimpleConverter/MainWindowViewModel.cs
+++ SimpleConverter/MainWindowViewModel.cs
@@ -25,21 +25,39 @@ namespace SimpleConverter
         /// </summary>
         private Contract.IPlugin _currentPlugin;
 
-        private ICommand _startConversionCommand;
+        /// <summary>
+        /// Output directory
+        /// </summary>
+        private string _outputPath;
+
+        /// <summary>
+        /// Command handlers
+        /// </summary>
+        private BaseCommand _startConversionCommand,
+                         _addFileCommand,
+                         _removeFileCommand,
+                         _browseCommand;
 
         /// <summary>
         /// Public constructor.
         /// </summary>
         public MainWindowViewModel()
         {
-            if(Factory.Loader.Instance.Plugins.Count<Contract.IPluginMetaData>() == 0)
+            if (Factory.Loader.Instance.Plugins.Count<Contract.IPluginMetaData>() == 0)
                 throw new Exception("No plugins available."); // todo use better (appropriate) exception
 
             Plugins = new ObservableCollection<Contract.IPluginMetaData>(Factory.Loader.Instance.Plugins);
             Messages = new ObservableCollection<ListMessage>();
+            Files = new ElementCollection<ListFile>();
             SelectedPlugin = Factory.Loader.Instance.Plugins.First<Contract.IPluginMetaData>();
 
             _startConversionCommand = new StartConversionCommand(this);
+            _addFileCommand = new AddFileCommand(this);
+            _removeFileCommand = new RemoveFileCommand(this);
+            _browseCommand = new BrowseCommand(this);
+
+            // load output path from user settings
+            _outputPath = Properties.Settings.Default.OutputPath;
         }
 
         /// <summary>
@@ -53,9 +71,20 @@ namespace SimpleConverter
         public ObservableCollection<ListMessage> Messages { get; private set; }
 
         /// <summary>
-        /// Handler for button click
+        /// Collection of files
+        /// </summary>
+        public ElementCollection<ListFile> Files { get; private set; }
+
+        /// <summary>
+        /// Handlers for button click
         /// </summary>
         public ICommand StartConversionCommand { get { return _startConversionCommand; } }
+
+        public ICommand AddFileCommand { get { return _addFileCommand; } }
+
+        public ICommand RemoveFileCommand { get { return _removeFileCommand; } }
+
+        public ICommand BrowseCommand { get { return _browseCommand;  } }
 
         /// <summary>
         /// Selected plugin in combobox
@@ -71,7 +100,7 @@ namespace SimpleConverter
                 _selectedPlugin = value;
 
                 // unregister message event
-                if(_currentPlugin != null)
+                if (_currentPlugin != null)
                     _currentPlugin.SendMessageEvent -= new Contract.SendMessageDelegate(OnSendMessage);
 
                 _currentPlugin = Factory.Loader.Instance[value.Key];
@@ -79,10 +108,37 @@ namespace SimpleConverter
                 // register message even with currently selected plugin
                 _currentPlugin.SendMessageEvent += new Contract.SendMessageDelegate(OnSendMessage);
 
+                // revalidate files in list
+                foreach (var item in Files)
+                {
+                    item.Valid = _currentPlugin.ValidateFile(item.Filepath);
+                }
+                Files.UpdateCollection();
+
                 // inform view about plugin change
                 InvokePropertyChanged("PluginView");
             }
         }
+
+        /// <summary>
+        /// Output directory path
+        /// </summary>
+        public string OutputPath
+        {
+            get { return _outputPath; }
+            set
+            {
+                _outputPath = value;
+                // remember path in user settings
+                Properties.Settings.Default.OutputPath = _outputPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        /// <summary>
+        /// Selected file in file list
+        /// </summary>
+        public ListFile SelectedFile { get; set; }
 
         /// <summary>
         /// Selected plugin settings view
@@ -103,16 +159,67 @@ namespace SimpleConverter
         private void OnSendMessage(string message, Contract.MessageLevel level = Contract.MessageLevel.INFO)
         {
             Messages.Add(new ListMessage(message, level));
-            // todo: invoke property change?
-            //InvokePropertyChanged("Messages");
         }
 
         #region Button Click Handlers
 
         public void StartConversionClicked()
         {
-            // hardcoded for debugging purposes
+            // hardcoded for debugging purposes - process in new thread
             _currentPlugin.ConvertDocument(@"D:\Programovani\VS.2010\SimpleConverter\_bordel\beamer.tex");
+        }
+
+        /// <summary>
+        /// Clicked on Add file button
+        /// </summary>
+        public void AddFileClicked()
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.Multiselect = true;
+            dlg.CheckFileExists = true;
+            // todo: set file extensions depending on plugin?
+
+            // Show open file dialog box
+            bool? result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                foreach (var item in dlg.FileNames)
+                {
+                    Files.Add(new ListFile(item, _currentPlugin.ValidateFile(item)));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Remove file from list button/key clicked
+        /// </summary>
+        public void RemoveFileClicked()
+        {
+            Files.Remove(SelectedFile);
+        }
+
+        /// <summary>
+        /// Browse for output folder
+        /// </summary>
+        public void BrowseClicked()
+        {
+            // WPF doesn't have folder browser dialog, so we have to use the one from Windows.Forms
+            System.Windows.Forms.FolderBrowserDialog dlg = new System.Windows.Forms.FolderBrowserDialog();
+            dlg.SelectedPath = OutputPath;
+            dlg.Description = "Select output directory.";   // todo: better description
+            dlg.ShowNewFolderButton = true;
+
+            System.Windows.Forms.DialogResult result = dlg.ShowDialog();
+
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                OutputPath = dlg.SelectedPath;
+                InvokePropertyChanged("OutputPath");
+            }
+
+            dlg.Dispose();
         }
 
         #endregion
@@ -143,13 +250,13 @@ namespace SimpleConverter
                 switch (level)
                 {
                     case SimpleConverter.Contract.MessageLevel.INFO:
-                        Icon = (System.Windows.Media.ImageSource) System.Windows.Application.Current.FindResource("iconInfo");
+                        Icon = (System.Windows.Media.ImageSource)System.Windows.Application.Current.FindResource("iconInfo");
                         break;
                     case SimpleConverter.Contract.MessageLevel.WARNING:
-                        Icon = (System.Windows.Media.ImageSource) System.Windows.Application.Current.FindResource("iconWarning");
+                        Icon = (System.Windows.Media.ImageSource)System.Windows.Application.Current.FindResource("iconWarning");
                         break;
                     case SimpleConverter.Contract.MessageLevel.ERROR:
-                        Icon = (System.Windows.Media.ImageSource) System.Windows.Application.Current.FindResource("iconError");
+                        Icon = (System.Windows.Media.ImageSource)System.Windows.Application.Current.FindResource("iconError");
                         break;
                     default:
                         throw new ArgumentException("Unknown message level.");
@@ -161,6 +268,50 @@ namespace SimpleConverter
             public System.Windows.Media.ImageSource Icon { get; private set; }
         }
 
-        #endregion
+        #endregion // Message wrapper class
+
+        #region File wrapper class
+
+        public class ListFile
+        {
+            public ListFile(string filename, bool valid = false)
+            {
+                Filename = System.IO.Path.GetFileName(filename); ;
+                Filepath = filename;
+                Valid = valid;
+            }
+
+            public string Filename { get; private set; }
+
+            public string Filepath { get; private set; }
+
+            public bool Valid { get; set; }
+
+            public string ValidColor
+            {
+                get
+                {
+                    if(Valid)
+                        return "PaleGreen";
+                    else
+                        return "LightSalmon";
+                }
+            }
+        }
+
+        #endregion // File wrapper class
+    }
+
+    /// <summary>
+    /// Observable collection with method for raising CollectionChanged event
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ElementCollection<T> : ObservableCollection<T>
+    {
+        public void UpdateCollection()
+        {
+            OnCollectionChanged(new System.Collections.Specialized.NotifyCollectionChangedEventArgs(
+                                System.Collections.Specialized.NotifyCollectionChangedAction.Reset));
+        }
     }
 }
