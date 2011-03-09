8dfc2eb41ea8f39816b43c4718e3a58d2e6cbf5a
Lukáš Černý <cerny.l@gmail.com>
2011-03-09 12:43:42 +0100

    Added some custom exceptions

---
+++ Factory/Exceptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SimpleConverter.Factory
+{
+    /// <summary>
+    /// Custom exception used for plugin handling errors
+    /// </summary>
+    [Serializable]
+    public class PluginLoaderException : Exception
+    {
+        public PluginLoaderException(string message)
+            : base(message) { }
+
+        public PluginLoaderException(string message, Exception innerException)
+            : base(message, innerException) { }
+        
+        protected PluginLoaderException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}
---
+++ plugins/Beamer2PPT/Exceptions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SimpleConverter.Plugin.Beamer2PPT
+{
+    /// <summary>
+    /// Custom exception used for PowerPoint manipulation errors
+    /// </summary>
+    [Serializable]
+    class PowerPointApplicationException : Exception
+    {
+        public PowerPointApplicationException(string message)
+            : base(message) { }
+
+        public PowerPointApplicationException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        protected PowerPointApplicationException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+
+    /// <summary>
+    /// Custom exception used for output document building errors
+    /// </summary>
+    [Serializable]
+    class DocumentBuilderException : Exception
+    {
+        public DocumentBuilderException(string message)
+            : base(message) { }
+
+        public DocumentBuilderException(string message, Exception innerException)
+            : base(message, innerException) { }
+
+        protected DocumentBuilderException(SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+    }
+}
