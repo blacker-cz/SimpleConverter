88fea519cbfe956ecfb115551cc2b2279e69b250
Lukáš Černý <cerny.l@gmail.com>
2011-04-16 20:22:36 +0200

    Added console project (uses NDesk.Options for parsing command line parameters)

---
+++ Console/Controller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleConverter.Console
+{
+    class Controller
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Controller()
+        {
+            if (Factory.Loader.Instance.Plugins.Count<Contract.IPluginMetaData>() == 0)
+                throw new Factory.PluginLoaderException("No plugins available.");
+        }
+
+        /// <summary>
+        /// List available plugins
+        /// </summary>
+        public void ListPlugins()
+        {
+            System.Console.WriteLine("Listing available plugins:\n");
+            System.Console.WriteLine("{0,10}   {1,-45}{2,10}", "Key", "Plugin name", "Version");
+            System.Console.WriteLine("-----------------------------------------------------------------------");
+
+            foreach (Contract.IPluginMetaData plugin in Factory.Loader.Instance.Plugins)
+            {
+                System.Console.WriteLine("{0,10}   {1,-45}{2,10}", plugin.Key, plugin.Name, plugin.Version);
+            }
+        }
+
+        /// <summary>
+        /// Convert files
+        /// </summary>
+        /// <param name="plugin_key">Plugin key</param>
+        /// <param name="output_dir">Output directory</param>
+        /// <param name="extra">List of files to convert</param>
+        /// <returns>0 if successfull; otherwise error number</returns>
+        public int Convert(string plugin_key, string output_dir, List<string> extra)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Console/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NDesk.Options;
+
+namespace SimpleConverter.Console
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            bool show_help = false, list_plugins = false;
+            string plugin_key = "", output_dir = "";
+
+            // no parameters given -> end execution
+            if (args.Length == 0)
+            {
+                System.Console.Write("SimpleConverter: ");
+                System.Console.WriteLine("No parameters given.");
+                System.Console.WriteLine("Try `sc --help' for more information.");
+                return 1;
+            }
+
+            var p = new OptionSet() {
+			    { "p|plugin=", "{KEY} of used conversion plugin.",  v => plugin_key = v },
+			    { "o|output=", "output {DIRECTORY} for converted files.", v => output_dir = v },
+			    { "h|help",  "show this message and exit", v => show_help = v != null },
+			    { "l|list",  "list available plugins and exit", v => list_plugins = v != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                System.Console.Write("SimpleConverter: ");
+                System.Console.WriteLine(e.Message);
+                System.Console.WriteLine("Try `sc --help' for more information.");
+                return 1;
+            }
+
+            // print help
+            if (show_help)
+            {
+                System.Console.WriteLine("Usage: sc [OPTIONS]+ files+");
+                System.Console.WriteLine("Convert document files using specified plugin.");
+                System.Console.WriteLine();
+                System.Console.WriteLine("Options:");
+                p.WriteOptionDescriptions(System.Console.Out);
+                return 0;
+            }
+
+            Controller controller;
+
+            try
+            {
+                controller = new Controller();
+            }
+            catch (Factory.PluginLoaderException ex)
+            {
+                System.Console.WriteLine(ex.Message);
+                return 1;
+            }
+
+            // list plugins
+            if (list_plugins)
+            {
+                controller.ListPlugins();
+                return 0;
+            }
+
+            if (plugin_key == null || plugin_key.Length == 0)
+            {
+                System.Console.Write("SimpleConverter: ");
+                System.Console.WriteLine("no plugin key given.");
+                System.Console.WriteLine("Try `sc --help' for more information.");
+                return 1;
+            }
+
+            return controller.Convert(plugin_key, output_dir, extra);
+        }
+
+    }
+}
